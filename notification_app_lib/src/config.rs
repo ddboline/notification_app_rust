24a8c9f1027b37501e45611af1f69d59e61f3651
Daniel Boline <ddboline@gmail.com>
2020-10-21 14:33:08 -0400

    checkpoint

--- notification_app_lib/src/config.rs
+++ notification_app_lib/src/config.rs
@@ -3,13 +3,17 @@ use serde::{Serialize, Deserialize};
 use url::Url;
 use derive_more::{Into, Deref, FromStr};
 use std::convert::TryFrom;
-use anyhow::Error;
+use anyhow::{format_err, Error};
+use std::path::{PathBuf, Path};
+use std::sync::Arc;
+use std::ops::Deref;
 
-#[derive(Debug, Deserialize)]
+#[derive(Default, Debug, Deserialize)]
 pub struct ConfigInner {
     pub telegram_bot_token: Option<StackString>,
     pub remote_url: Option<UrlWrapper>,
     pub remote_token: Option<StackString>,
+    pub api_tokens_path: Option<PathBuf>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, Into, PartialEq, Deref, FromStr)]
@@ -26,5 +30,44 @@ impl TryFrom<&str> for UrlWrapper {
     type Error = Error;
     fn try_from(item: &str) -> Result<Self, Self::Error> {
         item.parse().map_err(Into::into)
+    }
+}
+
+#[derive(Default, Debug, Clone)]
+pub struct Config(Arc<ConfigInner>);
+
+impl Config {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn init_config() -> Result<Self, Error> {
+        let fname = Path::new("config.env");
+        let config_dir = dirs::config_dir().ok_or_else(|| format_err!("No CONFIG directory"))?;
+        let default_fname = config_dir.join("notification_app_rust").join("config.env");
+
+        let env_file = if fname.exists() {
+            fname
+        } else {
+            &default_fname
+        };
+
+        dotenv::dotenv().ok();
+
+        if env_file.exists() {
+            dotenv::from_path(env_file).ok();
+        }
+
+        let conf: ConfigInner = envy::from_env()?;
+
+        Ok(Self(Arc::new(conf)))
+    }
+}
+
+impl Deref for Config {
+    type Target = ConfigInner;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
--- notification_app_lib/src/lib.rs
+++ notification_app_lib/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod config;
+pub mod ses_client;
 
 #[cfg(test)]
 mod tests {
