53d2b00db21af3c52e60422d207c6d4ff7422e47
Daniel Boline <ddboline@gmail.com>
2022-09-20 23:04:41 -0400

    tokio spawn

--- src/notification_app_api.rs
+++ src/notification_app_api.rs
@@ -4,6 +4,7 @@ use notification_app_api::app::start_app;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    start_app().await?;
-    Ok(())
+    tokio::spawn(async move { start_app().await })
+        .await
+        .unwrap()
 }
